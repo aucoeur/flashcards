e927fa2d9b665100cffed41ea4ff142e8a122809
Aucoeur Ngo <aucoeurngo@gmail.com>
2017-08-26 17:13:30 -0700

    added spacebar keypress event

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 $('.cardTemplate').click(function () {
   $(this).toggleClass('flipped');
 });
+
+$(window).keypress(function(e) {
+  if (e.keyCord == 32) {
+    $('.cardTemplate').toggleClass('flipped');
+  }
+});
