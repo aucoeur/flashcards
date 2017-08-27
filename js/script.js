606ac8ba96a833dabd76d531b8f9e7242e5e66a6
Aucoeur Ngo <aucoeurngo@gmail.com>
2017-08-27 14:37:50 -0700

    added array

--- js/script.js
+++ js/script.js
@@ -1,23 +1,39 @@
-$('.cardTemplate').click(function () {
+var defaultWord = {English: "English", Japanese: "日本語"}
+
+$('.englishVocab').text(defaultWord.English);
+$('.japaneseVocab').text(defaultWord.Japanese);
+
+$('div.cardTemplate').click(function () {
   $(this).toggleClass('flipped');
 });
 
 addEventListener("keydown", function(event) {
+  var vocabWords = [{English: "go", Romaji: "ikimasu (iku)", Hiragana: "いきます", Kanji: "行きます"},
+                    {English: "come", Romaji: "kimasu", Hiragana: "きます", Kanji: "来ます"},
+                    {English: "eat", Romaji: "tabemasu", Hiragana: "たべます", Kanji:"食べます"}
+                  ]
+
   if (event.keyCode == 32)
-    $('.cardTemplate').toggleClass('flipped');
+    $('div.cardTemplate').toggleClass('flipped');
   else if (event.keyCode == 39) {
-    function getRandomIntInc(min, max) {
+
+  function getRandomIntInc(min, max) {
       min = Math.ceil(min);
       max = Math.floor(max);
       return Math.floor(Math.random() * (max - min + 1)) + min;
       //The maximum is inclusive and the minimum is inclusive
     }
 
-    var mathRandom = getRandomIntInc(0,4);
+    var maxInt = vocabWords.length;
+    var i = getRandomIntInc(0,maxInt);
 
-    $('.englishVocab').text('Hi I am replace');
-    $('.japaneseVocab').text('こんにちわ');
+    $('.englishVocab').text(vocabWords[i].English);
+    $('.japaneseVocab').html(vocabWords[i].Romaji +" <br /> " + vocabWords[i].Hiragana +" | " + vocabWords[i].Kanji);
 
+  }
+  else if (event.keyCode == 37) {
+    $('.englishVocab').text(defaultWord.English);
+    $('.japaneseVocab').text(defaultWord.Japanese);
   }
   else {
     alert('You\'ve gon\' an\' done somefink off it now here')
