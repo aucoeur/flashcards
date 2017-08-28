f312a18bceb0d8a84dfc76012151e9faa8e0c40a
aucoeurngo <christine@library.caltech.edu>
2017-08-28 15:45:35 -0700

    rearraged code for order

--- sandbox/txt-to-JSarray/script.js
+++ sandbox/txt-to-JSarray/script.js
@@ -1,8 +1,8 @@
 function autoMagical() {
   var regEx = /(^.+$)\n(^.+$)\n(^.+$)\n(^.+$)\n/gm;
-  var inputText = document.getElementById("rawText").value;
   var subStr = `{English: "$1", Romaji: "$2", Hiragana: "$3", Kanji: "$4"},`;
-      // The substituted value will be contained in the result variable
+
+  var inputText = document.getElementById("rawText").value;
   var convertedText = inputText.replace(regEx, subStr);
 
   document.getElementById("results").innerHTML = convertedText;
