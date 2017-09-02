be3cb4976a32dae7dbe27ce226bfd1370432bdc6
Aucoeur Ngo <aucoeurngo@gmail.com>
2017-09-02 13:01:06 -0700

    added to kanjiJ1.js

--- js/kanjiJ1.js
+++ js/kanjiJ1.js
@@ -7,4 +7,17 @@ var kanjiJ1 = [{English: "one", Romaji: "ichi", Hiragana: "いち", Kanji: "一"
               {English: "seven", Romaji: "nana/shichi", Hiragana: "なな/しち", Kanji: "七"},
               {English: "eight", Romaji: "hachi", Hiragana: "はち", Kanji: "八"},
               {English: "nine", Romaji: "kyuu", Hiragana: "きゅう", Kanji: "九"},
-              {English: "ten", Romaji: "jyuu", Hiragana: "じゅう", Kanji: "十"},]
+              {English: "ten", Romaji: "jyuu", Hiragana: "じゅう", Kanji: "十"},
+              {English: "moon", Romaji: "gatsu/getsu", Hiragana: "がつ/げつ", Kanji: "月"},
+              {English: "day", Romaji: "nichi/bi", Hiragana: "にち/び", Kanji: "日"},
+              {English: "book", Romaji: "hon", Hiragana: "ほん", Kanji: "本"},
+              {English: "yen", Romaji: "en", Hiragana: "えん", Kanji: "円"},
+              {English: "big", Romaji: "ookii/dai", Hiragana: "おおきい/だい", Kanji: "大"},
+              {English: "school", Romaji: "gaku/manabu", Hiragana: "がく/まなぶ", Kanji: "学"},
+              {English: "life", Romaji: "ikiru/iru/sei", Hiragana: "せい", Kanji: "生"},
+              {English: "what", Romaji: "nan/nani", Hiragana: "なん/なに", Kanji: "何"},
+              {English: "white", Romaji: "shiro/shiroi", Hiragana: "しろ/しろい", Kanji: "白い"},
+              {English: "I", Romaji: "watashi", Hiragana: "わたし", Kanji: "私"},
+              {English: "dog", Romaji: "inu", Hiragana: "いぬ", Kanji: "犬"},
+              {English: "year", Romaji: "nen", Hiragana: "ねん", Kanji: "年"},
+              {English: "language", Romaji: "go", Hiragana: "ご", Kanji: "語"}]
